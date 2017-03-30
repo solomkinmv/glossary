34d78937d9451bbb9d2f6bff40c58b7ee507f79d
Maksym Solomkin <solomkinmv@gmail.com>
2017-03-31 01:10:00 +0300

    Update persistence layer to use simple studied words

--- persistence/src/main/java/io/github/solomkinmv/glossary/persistence/dao/WordDao.java
+++ persistence/src/main/java/io/github/solomkinmv/glossary/persistence/dao/WordDao.java
@@ -4,10 +4,13 @@ import io.github.solomkinmv.glossary.persistence.model.StudiedWord;
 import io.github.solomkinmv.glossary.persistence.model.Word;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Describes methods to interact with {@link StudiedWord} domain object.
  */
 public interface WordDao extends CRUDDao<Word, Long> {
     List<Word> search(String text);
+
+    Optional<Word> findByText(String text);
 }
