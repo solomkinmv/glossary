7f8cdae94df6efc11ed6a4808178240aabeb4c0d
Maksym Solomkin <solomkinmv@gmail.com>
2017-04-06 20:08:46 +0300

    Add word DTO validation

--- web/src/main/java/io/github/solomkinmv/glossary/web/dto/WordSetDto.java
+++ web/src/main/java/io/github/solomkinmv/glossary/web/dto/WordSetDto.java
@@ -2,12 +2,16 @@ package io.github.solomkinmv.glossary.web.dto;
 
 import lombok.Value;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Null;
 import java.util.List;
 
 @Value
 public class WordSetDto {
+    @Null
     Long id;
     String name;
     String description;
+    @Valid
     List<WordDto> words;
 }
