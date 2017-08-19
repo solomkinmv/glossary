52e73c73f2c54216a3f4d5d0f81fa4ca93871b83
Maksym Solomkin <solomkinmv@gmail.com>
2017-08-19 23:50:51 +0300

    Update wrapper script to check for spring profile

--- web/src/main/docker/wrapper.sh
+++ web/src/main/docker/wrapper.sh
@@ -1,6 +1,23 @@
 #!/bin/ash
 
-until nc -z -v -w30 $DATABASE_HOST $DATABASE_PORT
+
+# contains(string, substring)
+#
+# Returns 0 if the specified string contains the specified substring,
+# otherwise returns 1.
+contains() {
+    string="$1"
+    substring="$2"
+    if test "${string#*$substring}" != "$string"
+    then
+        return 0    # $substring is in $string
+    else
+        return 1    # $substring is not in $string
+    fi
+}
+
+db_profile="container"
+contains ${SPRING_PROFILES_ACTIVE} ${db_profile} && until nc -z -v -w30 $DATABASE_HOST $DATABASE_PORT
 do
   echo "Waiting for database connection..."
   # wait for 5 seconds before check again
