8fe0f1ada0cccb646cb15d935c6590a4e547f020
Maksym Solomkin <solomkinmv@gmail.com>
2018-02-14 00:05:17 +0200

    Storage service client

---
+++ storage-service-client/src/main/java/io/github/solomkinmv/glossary/storage/client/StoredType.java
@@ -0,0 +1,8 @@
+package io.github.solomkinmv.glossary.storage.client;
+
+/**
+ * Specifies type of object stored by {@link StorageService}.
+ */
+public enum StoredType {
+    IMG, SOUND
+}
---
+++ storage-service-client/src/main/java/io/github/solomkinmv/glossary/storage/client/config/StorageClientAutoConfiguration.java
@@ -0,0 +1,30 @@
+package io.github.solomkinmv.glossary.storage.client.config;
+
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.ribbon.FeignRibbonClientAutoConfiguration;
+import org.springframework.cloud.netflix.feign.support.SpringEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnProperty(prefix = "storage-client.library", name = "enabled",
+        havingValue = "true", matchIfMissing = true)
+@AutoConfigureAfter(FeignRibbonClientAutoConfiguration.class)
+@EnableFeignClients(basePackages = "io.github.solomkinmv.glossary.storage.client")
+public class StorageClientAutoConfiguration {
+
+    @Autowired
+    private ObjectFactory<HttpMessageConverters> messageConverters;
+
+    @Bean
+    public Encoder feignFormEncoder() {
+        return new SpringFormEncoder(new SpringEncoder(messageConverters));
+    }
+}
