d9cb27a160695387a67bd0bab90a4b7ee73d9c7d
Maksym Solomkin <solomkinmv@gmail.com>
2018-03-25 00:37:39 +0200

    Add caching

--- tts-service/src/main/java/io/github/solomkinmv/glossary/tts/controller/SpeechController.java
+++ tts-service/src/main/java/io/github/solomkinmv/glossary/tts/controller/SpeechController.java
@@ -2,6 +2,7 @@ package io.github.solomkinmv.glossary.tts.controller;
 
 import io.github.solomkinmv.glossary.tts.service.SpeechService;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ public class SpeechController {
 
     private final SpeechService speechService;
 
+    @Cacheable("speeches")
     @GetMapping
     public SpeechResult getSpeech(@RequestParam("text") String text) {
         return new SpeechResult(speechService.getSpeechRecord(text));
