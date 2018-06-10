9b543136abdf7bcb4ffacbc5e46d8edb57397a87
Maksym Solomkin <solomkinmv@gmail.com>
2018-06-10 15:49:05 +0300

    Add word controller documentation

--- words-service/src/test/java/io/github/solomkinmv/glossary/words/controller/WordSetControllerSystemTest.java
+++ words-service/src/test/java/io/github/solomkinmv/glossary/words/controller/WordSetControllerSystemTest.java
@@ -1,6 +1,5 @@
 package io.github.solomkinmv.glossary.words.controller;
 
-import io.github.solomkinmv.glossary.tts.client.TtsClient;
 import io.github.solomkinmv.glossary.tts.client.domain.SpeechResult;
 import io.github.solomkinmv.glossary.words.controller.dto.WordResponse;
 import io.github.solomkinmv.glossary.words.controller.dto.WordSetResponse;
@@ -9,19 +8,20 @@ import io.github.solomkinmv.glossary.words.persistence.repository.WordRepository
 import io.github.solomkinmv.glossary.words.service.word.WordMeta;
 import io.github.solomkinmv.glossary.words.service.wordset.WordSetMeta;
 import io.github.solomkinmv.glossary.words.service.wordset.WordSetService;
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static io.github.solomkinmv.glossary.words.persistence.domain.WordStage.NOT_LEARNED;
 import static java.lang.String.valueOf;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.hasSize;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,18 +29,9 @@ public class WordSetControllerSystemTest extends BaseTest {
 
     @Autowired
     private WordSetService wordSetService;
+
     @Autowired
     private WordRepository wordRepository;
-    @MockBean
-    private TtsClient ttsClient;
-
-    @Before
-    public void setUpMockMvc() {
-        super.setUpMockMvc();
-
-        when(ttsClient.getSpeech(any(String.class)))
-                .thenReturn(new SpeechResult("url"));
-    }
 
     @Test
     public void createsWordSet() throws Exception {
@@ -60,37 +51,58 @@ public class WordSetControllerSystemTest extends BaseTest {
 
     @Test
     public void getsAllWordSets() throws Exception {
-        String name1 = "word-set-name1";
-        String name2 = "word-set-name2";
-        String description = "desc";
-        WordSetMeta wordSetMeta1 = new WordSetMeta(userId.get(), name1, description);
-        WordSetMeta wordSetMeta2 = new WordSetMeta(userId.get(), name2, description);
-
-        long wsId1 = createWordSet(wordSetMeta1);
-        long wsId2 = createWordSet(wordSetMeta2);
-
         mockMvc.perform(get("/word-sets/")
                                 .param("userId", valueOf(userId.get())))
+               .andDo(print())
                .andExpect(status().isOk())
-               .andExpect(jsonPath("$[0].id").value(wsId1))
-               .andExpect(jsonPath("$[0].name").value(name1))
-               .andExpect(jsonPath("$[1].id").value(wsId2))
-               .andExpect(jsonPath("$[1].name").value(name2));
+               .andExpect(jsonPath("$[*].id", containsInAnyOrder((int) wordSet1.getId(),
+                                                                 (int) wordSet2.getId())))
+               .andExpect(jsonPath("$[*].name", containsInAnyOrder(wordSet1.getName(),
+                                                                   wordSet2.getName())))
+               .andDo(documentationHandler.document(
+                       requestParameters(
+                               parameterWithName("userId").description("User id to get all word sets")
+                       ),
+                       responseFields(
+                               fieldWithPath("[]id").description("Word Set id"),
+                               fieldWithPath("[]name").description("Word Set name"),
+                               fieldWithPath("[]description").description("Word Set description"),
+                               fieldWithPath("[]words[].id").description("Word id"),
+                               fieldWithPath("[]words[].text").description("Word text"),
+                               fieldWithPath("[]words[].translation").description("Word translation"),
+                               fieldWithPath("[]words[].stage").description("Word learning stage"),
+                               fieldWithPath("[]words[].image").description("Word image url"),
+                               fieldWithPath("[]words[].sound").description("Word pronunciation sound url")
+                       )
+               ));
     }
 
     @Test
     public void getsWordSetById() throws Exception {
         String name = "word-set-name";
         String description = "desc";
-        WordSetMeta wordSetMeta = new WordSetMeta(userId.get(), name, description);
 
-        long wordSetId = createWordSet(wordSetMeta);
-
-        performGetWordSetById(wordSetId)
+        performGetWordSetById(wordSet1.getId())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(wordSetId))
+                .andExpect(jsonPath("$.id").value(wordSet1.getId()))
                 .andExpect(jsonPath("$.name").value(name))
-                .andExpect(jsonPath("$.description").value(description));
+                .andExpect(jsonPath("$.description").value(description))
+                .andDo(documentationHandler.document(
+                        pathParameters(
+                                parameterWithName("wordSetId").description("Word Set id")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("Word Set id"),
+                                fieldWithPath("name").description("Word Set name"),
+                                fieldWithPath("description").description("Word Set description"),
+                                fieldWithPath("words[].id").description("Word id"),
+                                fieldWithPath("words[].text").description("Word text"),
+                                fieldWithPath("words[].translation").description("Word translation"),
+                                fieldWithPath("words[].stage").description("Word learning stage"),
+                                fieldWithPath("words[].image").description("Word image url"),
+                                fieldWithPath("words[].sound").description("Word pronunciation sound url")
+                        )
+                ));
     }
 
     @Test
@@ -117,7 +129,28 @@ public class WordSetControllerSystemTest extends BaseTest {
                 .andExpect(jsonPath("$.words[0].translation").value(wordTranslation))
                 .andExpect(jsonPath("$.words[0].image").value(imageUrl))
                 .andExpect(jsonPath("$.words[0].sound").value(speechUrl))
-                .andExpect(jsonPath("$.words[0].stage").value(NOT_LEARNED.name()));
+                .andExpect(jsonPath("$.words[0].stage").value(NOT_LEARNED.name()))
+                .andDo(documentationHandler.document(
+                        requestFields(
+                                fieldWithPath("text").description("Word to add"),
+                                fieldWithPath("translation").description("Word to add"),
+                                fieldWithPath("image").description("New word's image url")
+                        ),
+                        pathParameters(
+                                parameterWithName("wordSetId").description("Word Set id")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("Word Set id"),
+                                fieldWithPath("name").description("Word Set name"),
+                                fieldWithPath("description").description("Word Set description"),
+                                fieldWithPath("words[].id").description("Word id"),
+                                fieldWithPath("words[].text").description("Word text"),
+                                fieldWithPath("words[].translation").description("Word translation"),
+                                fieldWithPath("words[].stage").description("Word learning stage"),
+                                fieldWithPath("words[].image").description("Word image url"),
+                                fieldWithPath("words[].sound").description("Word pronunciation sound url")
+                        )
+                ));
     }
 
     @Test
@@ -131,8 +164,17 @@ public class WordSetControllerSystemTest extends BaseTest {
         WordSetResponse wordSetResponse = addWordToWordSet(wordSetId, new WordMeta("word2", "translation", "img-url"));
 
         mockMvc.perform(delete("/word-sets/{wordSetId}", wordSetId))
-               .andExpect(status().isOk());
+               .andExpect(status().isOk())
+               .andDo(documentationHandler.document(
+                       pathParameters(
+                               parameterWithName("wordSetId").description("Word Set id")
+                       )
+               ));
 
+        verifyThatWordWithAllCorrespondingWordsHaveBeenDeleted(wordSetId, wordSetResponse);
+    }
+
+    private void verifyThatWordWithAllCorrespondingWordsHaveBeenDeleted(long wordSetId, WordSetResponse wordSetResponse) throws Exception {
         performGetWordSetById(wordSetId)
                 .andExpect(status().isNotFound());
 
@@ -165,7 +207,28 @@ public class WordSetControllerSystemTest extends BaseTest {
                .andExpect(jsonPath("$.id").value(wordSetId))
                .andExpect(jsonPath("$.name").value(updatedName))
                .andExpect(jsonPath("$.description").value(updatedDescription))
-               .andExpect(jsonPath("$.words", hasSize(2)));
+               .andExpect(jsonPath("$.words", hasSize(2)))
+               .andDo(documentationHandler.document(
+                       requestFields(
+                               fieldWithPath("userId").description("Appropriate user id"),
+                               fieldWithPath("name").description("Word Set's name"),
+                               fieldWithPath("description").description("Word Set's description")
+                       ),
+                       pathParameters(
+                               parameterWithName("wordSetId").description("Word Set id")
+                       ),
+                       responseFields(
+                               fieldWithPath("id").description("Word Set id"),
+                               fieldWithPath("name").description("Word Set name"),
+                               fieldWithPath("description").description("Word Set description"),
+                               fieldWithPath("words[].id").description("Word id"),
+                               fieldWithPath("words[].text").description("Word text"),
+                               fieldWithPath("words[].translation").description("Word translation"),
+                               fieldWithPath("words[].stage").description("Word learning stage"),
+                               fieldWithPath("words[].image").description("Word image url"),
+                               fieldWithPath("words[].sound").description("Word pronunciation sound url")
+                       )
+               ));
     }
 
     @Test
@@ -181,12 +244,20 @@ public class WordSetControllerSystemTest extends BaseTest {
         WordResponse wordThatShouldStay = wordSetResponse.getWords().get(1);
 
         mockMvc.perform(delete("/word-sets/{wordSetId}/words/{wordId}", wordSetId, wordToDelete.getId()))
-               .andExpect(status().isOk());
+               .andExpect(status().isOk())
+               .andDo(documentationHandler.document(
+                       pathParameters(
+                               parameterWithName("wordSetId").description("Word Set id"),
+                               parameterWithName("wordId").description("Word id")
+                       )
+               ));
 
+        verifyThatWordHasBeenDelitedFromWordSet(wordSetId, wordThatShouldStay);
+    }
+
+    private void verifyThatWordHasBeenDelitedFromWordSet(long wordSetId, WordResponse wordThatShouldStay) throws Exception {
         WordSetResponse response = getWordSetById(wordSetId);
 
-        assertThat(response.getName()).isEqualTo(name);
-        assertThat(response.getDescription()).isEqualTo(description);
         assertThat(response.getWords()).hasSize(1);
         assertThat(response.getWords()).containsOnly(wordThatShouldStay);
     }
