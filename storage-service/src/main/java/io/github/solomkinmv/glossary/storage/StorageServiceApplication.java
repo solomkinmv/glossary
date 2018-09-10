5efdfc12a507643f8e982be49bf4a42a319f90da
Maksym Solomkin <solomkinmv@gmail.com>
2018-09-10 19:49:19 +0300

    Enable circuit breaker for storage-service

--- storage-service/src/main/java/io/github/solomkinmv/glossary/storage/StorageServiceApplication.java
+++ storage-service/src/main/java/io/github/solomkinmv/glossary/storage/StorageServiceApplication.java
@@ -2,11 +2,11 @@ package io.github.solomkinmv.glossary.storage;
 
 import io.github.solomkinmv.glossary.storage.properties.StorageProperties;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringCloudApplication
 @EnableDiscoveryClient
 @EnableConfigurationProperties(StorageProperties.class)
 public class StorageServiceApplication {
