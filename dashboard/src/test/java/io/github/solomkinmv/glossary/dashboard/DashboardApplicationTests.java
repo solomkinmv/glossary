5b99ae360bee1d61915b6accd7d92ffa4b3d562e
Maksym Solomkin <solomkinmv@gmail.com>
2018-09-10 20:28:52 +0300

    Fix tests for dashboard

--- dashboard/src/test/java/io/github/solomkinmv/glossary/dashboard/DashboardApplicationTests.java
+++ dashboard/src/test/java/io/github/solomkinmv/glossary/dashboard/DashboardApplicationTests.java
@@ -1,13 +1,19 @@
 package io.github.solomkinmv.glossary.dashboard;
 
+import com.netflix.discovery.EurekaClient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class DashboardApplicationTests {
+    @MockBean
+    EurekaClient eurekaClient;
 
     @Test
     public void contextLoads() {
