a2c4dd7161af786d68a912133ea8f3173b97e0ea
Maksym Solomkin <solomkinmv@gmail.com>
2018-09-23 19:17:48 +0300

    Add hystrix-dashboard kuber service

--- storage-service-client/src/main/java/io/github/solomkinmv/glossary/storage/client/StorageClient.java
+++ storage-service-client/src/main/java/io/github/solomkinmv/glossary/storage/client/StorageClient.java
@@ -2,7 +2,11 @@ package io.github.solomkinmv.glossary.storage.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Optional;
@@ -10,9 +14,7 @@ import java.util.Optional;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 
-@FeignClient(
-        name = "storage-service"
-)
+@FeignClient(name = "storage-service")
 public interface StorageClient {
 
     String UPLOAD_IMG_KEY = "file";
