a53a3e5c8bfcfc98142f17b49480af45cd5bb13c
Maksym Solomkin <solomkinmv@gmail.com>
2018-09-23 23:27:38 +0300

    Implement statistics-service skeleton

---
+++ statistics-service/src/main/java/io/github/solomkinmv/glossary/statistics/StatisticsServiceApplication.java
@@ -0,0 +1,12 @@
+package io.github.solomkinmv.glossary.statistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class StatisticsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatisticsServiceApplication.class, args);
+    }
+}
