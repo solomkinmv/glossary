61ede41c80726b6c58f0064016dc0b0524344860
Maksym Solomkin <solomkinmv@gmail.com>
2018-09-23 17:40:04 +0300

    Remove translate client fallback due to caching problems

--- translate-service/src/main/java/io/github/solomkinmv/translateservice/client/YandexTranslateClient.java
+++ translate-service/src/main/java/io/github/solomkinmv/translateservice/client/YandexTranslateClient.java
@@ -1,35 +1,16 @@
 package io.github.solomkinmv.translateservice.client;
 
-import feign.hystrix.FallbackFactory;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import static java.util.Collections.emptyList;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-@FeignClient(url = "https://translate.yandex.net", name = "yandex-translate", fallbackFactory = YandexTranslateClientFallbackFactory.class)
+@FeignClient(url = "https://translate.yandex.net", name = "yandex-translate")
 public interface YandexTranslateClient {
 
-    @PostMapping(value = "/api/v1.5/tr.json/translate",
-            consumes = APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/api/v1.5/tr.json/translate", consumes = APPLICATION_JSON_VALUE)
     YandexTranslationResult translate(@RequestParam("key") String key,
                                       @RequestParam("text") String text,
                                       @RequestParam("lang") String translationDirection);
-}
-
-@Slf4j
-@Component
-class YandexTranslateClientFallbackFactory implements FallbackFactory<YandexTranslateClient> {
-
-    @Override
-    public YandexTranslateClient create(Throwable cause) {
-        return (key, text, translationDirection) -> {
-            log.warn("Failed to get translation result [key: {}, text: {}, translationDirection: {}]",
-                     key, text, translationDirection, cause);
-            return new YandexTranslationResult((short) 200, translationDirection, emptyList());
-        };
-    }
 }
