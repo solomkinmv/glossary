c88a27073860cc230932ebcc70cf70e2034d8785
Maksym Solomkin <solomkinmv@gmail.com>
2018-09-23 15:36:02 +0300

    Fix file adapter for storage service

--- storage-service/src/main/java/io/github/solomkinmv/glossary/storage/filename/FilenameAdapter.java
+++ storage-service/src/main/java/io/github/solomkinmv/glossary/storage/filename/FilenameAdapter.java
@@ -2,25 +2,21 @@ package io.github.solomkinmv.glossary.storage.filename;
 
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
-
 @Service
 public class FilenameAdapter {
 
     public String adapt(String originalFilename) {
-        return insertIdBeforeExtension(
-                removeIllegalCharacters(originalFilename));
+        return removeIllegalCharactersExceptDotBeforeFileExtension(originalFilename);
     }
 
     private String removeIllegalCharacters(String filename) {
-        return filename.replace(" ", "")
-                       .replace("/", "")
-                       .replace("\\", "");
+        return filename.replaceAll("\\s+", "-")
+                       .replaceAll("[^a-zA-Z\\d\\-]", "");
     }
 
-    private String insertIdBeforeExtension(String filename) {
+    private String removeIllegalCharactersExceptDotBeforeFileExtension(String filename) {
         int dotIndex = filename.lastIndexOf('.');
 
-        return filename.substring(0, dotIndex) + UUID.randomUUID() + filename.substring(dotIndex);
+        return removeIllegalCharacters(filename.substring(0, dotIndex)) + filename.substring(dotIndex);
     }
 }
