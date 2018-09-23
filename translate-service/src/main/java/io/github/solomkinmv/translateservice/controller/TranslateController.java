ad4b2f258bf778b746dcc08c47056874b21cd63f
Maksym Solomkin <solomkinmv@gmail.com>
2018-09-23 18:37:47 +0300

    Add logs for translate controller

--- translate-service/src/main/java/io/github/solomkinmv/translateservice/controller/TranslateController.java
+++ translate-service/src/main/java/io/github/solomkinmv/translateservice/controller/TranslateController.java
@@ -4,12 +4,14 @@ import io.github.solomkinmv.translateservice.domain.Language;
 import io.github.solomkinmv.translateservice.domain.TranslateResult;
 import io.github.solomkinmv.translateservice.service.Translator;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @AllArgsConstructor
 @RestController
 public class TranslateController {
@@ -21,6 +23,7 @@ public class TranslateController {
     public ResponseEntity<TranslateResult> translate(@RequestParam("text") String text,
                                                      @RequestParam(value = "source", required = false) Language source,
                                                      @RequestParam("target") Language translationDirection) {
+        log.info("Translating text \"{}\" from {} language to {}", text, source, translationDirection);
 
         if (source == null) {
             return translator.execute(text, translationDirection)
