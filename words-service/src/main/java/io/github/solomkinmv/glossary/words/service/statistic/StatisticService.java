26d97ef2976dc5481edc0916752504e47e289404
Maksym Solomkin <solomkinmv@gmail.com>
2018-10-12 01:02:24 +0300

    Add words-service logs for stats

--- words-service/src/main/java/io/github/solomkinmv/glossary/words/service/statistic/StatisticService.java
+++ words-service/src/main/java/io/github/solomkinmv/glossary/words/service/statistic/StatisticService.java
@@ -1,10 +1,12 @@
 package io.github.solomkinmv.glossary.words.service.statistic;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @AllArgsConstructor
 public class StatisticService {
@@ -12,6 +14,7 @@ public class StatisticService {
     private final Source statisticsSource;
 
     public void reportStats(LearningResultMessage learningResultMessage) {
+        log.info("Reporting stats: {}", learningResultMessage);
         statisticsSource.output()
                         .send(MessageBuilder.withPayload(learningResultMessage).build());
     }
